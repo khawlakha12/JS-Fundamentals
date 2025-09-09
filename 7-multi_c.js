0dab41956829325e95b1844c7b73b1eeb6c50ee9
khawlakha12 <kharbouchikhawla603@gmail.com>
2025-09-09 18:02:46 +0200

    test 7 modif

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,4 @@
 const arg = process.argv[2];
 const count = parseInt(arg, 10);
-
-if (isNaN(count) || count <= 0) {
-  console.log("Missing number of occurrences");
-} else {
-  for (let i = 0; i < count; i++) {
-    console.log("C is fun");
-  }
-}
+if (!count || count < 1) console.log("Missing number of occurrences");
+else for (let i = 0; i < count; i++) console.log("C is fun");
